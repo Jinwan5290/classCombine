f7243cc40560c6d1d9c0a5f413904ac1b33ae1f5
jinwanyang <jinwanyang@tencent.com>
2020-09-23 16:55:22 +0800

    m

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+interface ClassArgs {
+  [className: string]: boolean;
+}
+
+export default (args: ClassArgs): string => {
+  if (!Object.keys(args).length) return "";
+
+  return (
+    Object.keys(args).reduce((combineClass, curClass) => {
+      if (args[curClass] === true) combineClass += " " + curClass;
+      return combineClass;
+    }) || ""
+  );
+};
+
+// created by jinwan
